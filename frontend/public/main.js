fa941d6373cfb109df6db18b204243fbaecdf459
Thomas <henwoodthomas95@gmail.com>
2025-06-13 13:06:33 +0200

    live reload setup

---
+++ frontend/public/main.js
@@ -0,0 +1,50 @@
+import { renderHome } from "./views/home.js";
+import { renderProfil } from "./views/profil.js";
+import { renderChat } from "./views/chat.js";
+import { renderAuth } from "./views/auth.js";
+import { navBar } from "./components/navbar.js";
+// Barre de navigation
+const body = document.body;
+body.prepend(navBar());
+// Différentes pages
+function render(path) {
+    switch (path) {
+        case '/':
+            renderHome();
+            break;
+        case '/profil':
+            renderProfil();
+            break;
+        case '/chat':
+            renderChat();
+            break;
+        case '/auth':
+            renderAuth();
+            break;
+        default:
+            document.getElementById("app").innerHTML = `<h1>Page non trouvée</h1>`;
+    }
+}
+// Intercepter les clics sur les liens
+document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.tagName === 'A') {
+        const anchor = target;
+        const href = anchor.getAttribute('href');
+        if (href && href.startsWith('/')) {
+            e.preventDefault(); // stop le rechargement
+            navigate(href); // navigation SPA
+        }
+    }
+});
+// SPA
+function navigate(path) {
+    window.history.pushState({}, '', path);
+    render(path);
+}
+// Gérer les retours en arrière du navigateur
+window.addEventListener('popstate', () => {
+    render(window.location.pathname);
+});
+// Initialisation
+render(window.location.pathname);
--- frontend/src/views/home.ts
+++ frontend/src/views/home.ts
@@ -1,2 +1,6 @@
 export function renderHome(): void {
+	const app = document.getElementById('app')!;
+	app.innerHTML = `
+		<h1 class="text-4xl text-center">Bienvenue sur la page d’accueil</h1>
+	`;
 }
